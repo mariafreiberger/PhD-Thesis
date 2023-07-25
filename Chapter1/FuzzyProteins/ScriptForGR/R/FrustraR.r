2f564aede5d629223e6fe443be89819d323819c1
Maria Ines Freiberger <37904042+mariafreiberger@users.noreply.github.com>
2023-07-25 11:08:40 -0300

    Delete Chapter1/FuzzyProteins directory

--- Chapter1/FuzzyProteins/ScriptForGR/R/FrustraR.r
+++
@@ -1,9 +0,0 @@
-library(frustratometeR)
-
-PdbsDir <- "/Fuzzy/"
-ResultsDir <- "/Fuzzy/"
-
-# Calculate frustration of Pdbs contained in PdbDir
-dir_frustration(PdbsDir = PdbsDir, Mode = "configurational", ResultsDir = ResultsDir)
-dir_frustration(PdbsDir = PdbsDir, Mode = "mutational", ResultsDir = ResultsDir)
-dir_frustration(PdbsDir = PdbsDir, Mode = "singleresidue", ResultsDir = ResultsDir)
